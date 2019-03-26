d19efc15eef4e709e540260fa3c4a2da888ad4e2
bene802 <wangzetong94@gmail.com>
2019-03-26 17:22:08 -0400

    modal

--- frontend/src/components/Edit.jsx
+++ frontend/src/components/Edit.jsx
@@ -3,7 +3,52 @@ import React, { Component } from "react";
 class Edit extends Component {
   state = {};
   render() {
-    return "";
+    return (
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Edit Jewel
+              </h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>
+                <span className="modal-lable">Title:</span>
+              </p>
+              <p>
+                <span className="modal-lable">Content</span>
+              </p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+              Save changes
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
